0819041fef7fa826fdcda76131967cc13761d118
NovusEdge <novusedge0@gmail.com>
2022-04-08 15:49:55 +0430

    Moving source code to src

---
+++ src/auth.go
@@ -0,0 +1,11 @@
+package wbyte
+
+/*
+// Author: Aliasgar Khimani (NovusEdge)
+// Project: github.com/ARaChn3/web-byte
+//
+// Copyright: MPL 2.0
+// See the LICENSE file for more info.
+//
+// All Rights Reserved
+*/
